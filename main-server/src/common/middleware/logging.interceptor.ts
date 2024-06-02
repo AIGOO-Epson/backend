6427276e47f6efd375139df05050e9a61fc3c62b
hoontou <hoontou@gmail.com>
2024-06-02 10:18:05 +0900

    글로벌 로깅 추가

---
+++ main-server/src/common/middleware/logging.interceptor.ts
@@ -0,0 +1,87 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Logger,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(LoggingInterceptor.name);
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler
+  ): Observable<any> | Promise<Observable<any>> {
+    const req = context.switchToHttp().getRequest();
+    const { method, path: url, headers, connection, body } = req;
+    const now = Date.now();
+    const ipAddress = headers['x-forwarded-for'] || connection.remoteAddress;
+    this.logger.log(
+      `${method} ${url}: ${context.getClass().name} ${
+        context.getHandler().name
+      }`
+    );
+
+    Object.keys(body).length > 0 && this.logger.debug('Request body:', body);
+
+    return next.handle().pipe(
+      tap((res) => {
+        const response = context.switchToHttp().getResponse();
+        const { statusCode } = response;
+
+        this.logger.log(
+          `req from ${ipAddress}, ${method} ${url} ${statusCode}: ${Date.now() - now}ms`
+        );
+
+        this.logger.debug('Response:', res);
+      })
+    );
+  }
+}
+
+// import {
+//   Injectable,
+//   NestInterceptor,
+//   ExecutionContext,
+//   CallHandler,
+//   Logger,
+// } from '@nestjs/common';
+// import { Observable } from 'rxjs';
+// import { tap } from 'rxjs/operators';
+
+// @Injectable()
+// export class LoggingInterceptor implements NestInterceptor {
+//   private readonly logger = new Logger(LoggingInterceptor.name);
+
+//   intercept(
+//     context: ExecutionContext,
+//     next: CallHandler,
+//   ): Observable<any> | Promise<Observable<any>> {
+//     const req = context.switchToHttp().getRequest();
+//     const userAgent = req.get('user-agent') || '';
+//     const { ip, method, path: url } = req;
+//     const now = Date.now();
+
+//     this.logger.log(
+//       `${method} ${url} ${userAgent} ${ip}: ${context.getClass().name} ${
+//         context.getHandler().name
+//       }`,
+//     );
+
+//     return next.handle().pipe(
+//       tap((res) => {
+//         const response = context.switchToHttp().getResponse();
+//         const { statusCode } = response;
+//         this.logger.log(
+//           `${method} ${url} ${statusCode}: ${Date.now() - now}ms`,
+//         );
+
+//         this.logger.debug('Response:', res);
+//       }),
+//     );
+//   }
+// }
--- main-server/src/main.ts
+++ main-server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/middleware/logging.interceptor';
+import { Logger } from '@nestjs/common';
+const logger = new Logger('NestJS Application');
+const PORT = 3000;
+const EXPOSE_PORT = process.env.EXPOSE_PORT ?? 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.enableCors();
+
+  await app.listen(PORT);
+
+  logger.log(`main-server listening on`);
+  logger.log(`expose to local : ${EXPOSE_PORT} PORT`);
 }
 bootstrap();
